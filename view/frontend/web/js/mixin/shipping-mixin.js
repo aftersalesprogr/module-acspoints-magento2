31b94fc06acdd51bdaec99ae3e142a81dc4b3e2a
aftersalesprogr <106532474+aftersalesprogr@users.noreply.github.com>
2025-08-27 12:27:09 +0300

    2.0.0

--- view/frontend/web/js/mixin/shipping-mixin.js
+++ view/frontend/web/js/mixin/shipping-mixin.js
@@ -12,11 +12,10 @@ define([
             },
             validateShippingInformation: function () {
                 const isValid = this._super();
-                console.log('shipping-mixin: validate shipping')
                 if (isValid && this.isSelected() && this.isSelected() === 'AfterSalesProGrAcsPoints_courier') {
                     const val = $('#acs_pp_point_id').val();
-                    console.log('shipping-mixin: validate point')
-                    if (!val || val === '') {
+                    const valSlug = $('#acs_pp_point_slug').val();
+                    if (!val || val === '' || !valSlug || valSlug === '') {
                         this.errorValidationMessage(
                             $t('The shipping method is missing. Select the shipping method and try again.')
                         );
